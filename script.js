50900d65a5c356ab572eb32e09cad11e89661366
ConfusedGerman <okehermanns11@gmail.com>
2023-09-09 13:36:34 +0200

    Create a book with the users input and push it into the array

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ function book(title, author, pages, read) {
 
 }
 const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet.');
+
+function addBookToLibrary(form) {
+    var authorInput = form.author.value;
+    var titleinput = form.title.value;
+    var pagesInput = form.pages.value;
+    var readInput = form.status.value;
+    const usersBook = new book(authorInput, titleinput, pagesInput, readInput);
+    myLibrary.push(usersBook);
+}

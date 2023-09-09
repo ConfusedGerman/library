993254c013a991612d28501935c27d7ceb8308f0
ConfusedGerman <okehermanns11@gmail.com>
2023-09-09 23:16:17 +0200

    Create function to display books in table, recreate table with every update

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 const myLibrary = [];
 
-function book(title, author, pages, read) {
+function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-    }
-
-
 }
-const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet.');
 
 function addBookToLibrary(form) {
-    var authorInput = form.author.value;
-    var titleinput = form.title.value;
-    var pagesInput = form.pages.value;
-    var readInput = form.status.value;
-    const usersBook = new book(authorInput, titleinput, pagesInput, readInput);
+    let authorInput = form.author.value;
+    let titleInput = form.title.value;
+    let pagesInput = form.pages.value;
+    let readInput = form.status.value;
+    const usersBook = new Book(authorInput, titleInput, pagesInput, readInput);
     myLibrary.push(usersBook);
+    displayBooks();
     //TODO: readInput value is always "on"
 }
 
-function displayBooks (myLibrary) {
+function displayBooks () {
     if (myLibrary.length < 1) {
         return;
     }
-    else {
-        
+    
+    let tbody = document.querySelector('tbody');
+    // Clear the table before re-populating it
+    tbody.innerHTML = '';
+
+    for (let book of myLibrary) {
+        let row = tbody.insertRow();
+        for (let key in book) {
+            if (book.hasOwnProperty(key)) {
+                let cell = row.insertCell();
+                cell.appendChild(document.createTextNode(book[key]));
+            }
+        }
     }
 }

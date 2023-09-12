d05300560f7e4017379455528bb7edc481798729
ConfusedGerman <okehermanns11@gmail.com>
2023-09-12 22:39:08 +0200

    Add data attribute to read button

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ function displayBooks() {
                 var readButton = document.createElement('button');
                 readButton.className = 'readButton';
                 readButton.innerText = read.value;
+                btn.setAttribute('data-book-read-number', book.number);
                 cell.appendChild(readButton);
             }
             else if (key === "title" || key === "author" || key === "pages") {
@@ -105,6 +106,6 @@ function changeStatus() {
     const readBtns = document.querySelectorAll('.readButton');
     readBtns.forEach((button) => 
     button.addEventListener('click', () => {
-        Book.isRead();
+        button.isRead();
     }))
 }
